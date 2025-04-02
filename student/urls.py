efe01d00ab8f986d10cfcfe12010dcf96acb35f6
Darshan6424 <kawasotiho@gmail.com>
2025-04-02 11:24:32 +0545

    making db using django orm

--- student/urls.py
+++ student/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('students/', include("student.urls")),
-
+    path('',views.test, name="test")
 ]
